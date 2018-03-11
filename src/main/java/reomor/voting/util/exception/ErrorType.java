f6702c274442224cc2662ff70c5c95996355a736
Rimskiy <reomor@gmail.com>
2018-03-11 13:20:01 +0300

    error handling in rest

---
+++ src/main/java/reomor/voting/util/exception/ErrorType.java
@@ -0,0 +1,7 @@
+package reomor.voting.util.exception;
+
+public enum ErrorType {
+    APP_ERROR,
+    DATA_NOT_FOUND,
+    DATA_ERROR
+}
--- src/main/java/reomor/voting/util/exception/NotFoundException.java
+++ src/main/java/reomor/voting/util/exception/NotFoundException.java
@@ -1,5 +1,9 @@
 package reomor.voting.util.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No data found")
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
         super(message);
